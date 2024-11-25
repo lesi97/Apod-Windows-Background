2c41fbc7b5b3473eff58145d3884198e80b2ae68
lesi97 <lesi97@hotmail.co.uk>
2024-11-25 22:22:28 +0000

    simplified script to not depend on python, updated readme

--- Apod.ps1
+++ Apod.ps1
@@ -1,171 +1,85 @@
+$taskName = "Apod"
+$scheduledTaskExists = Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue
 
-If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-{
-    # Relaunch the script with administrator rights
-    Start-Process powershell.exe -Verb RunAs -ArgumentList $MyInvocation.MyCommand.Definition
-    Exit
-}
+if ($scheduledTaskExists -eq $null) {
 
-Add-Type -AssemblyName System.Windows.Forms
+    $scriptPath = $MyInvocation.MyCommand.Path
 
-$userDownloads = "${env:USERPROFILE}\Downloads"
-cd $userDownloads
-$userDocuments = "${env:USERPROFILE}\Documents"
-$userPictures = "${env:USERPROFILE}\Pictures"
-$pythonDirs = "${env:LOCALAPPDATA}\Programs\Python"
+    $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    if (-not $isAdmin) {
+        Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`"" -Verb RunAs
+        Start-Sleep 60
+        exit
+    }
 
-$pythonExe = Get-ChildItem -Path $pythonDirs -Filter "python.exe" -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1
+    $action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c start /min powershell.exe -NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`""
+    $trigger = New-ScheduledTaskTrigger -AtLogOn
+    $principal = New-ScheduledTaskPrincipal -UserId $env:USERNAME -LogonType Interactive -RunLevel Highest
+    $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -Hidden
 
-
-if ($null -eq $pythonExe) {
-    Write-Output "Python not installed, downloading Python from https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe"
-    $pythonDownloadLink = "https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe"
-    $pythonLocalName = Join-Path $userDownloads "Python-3.12.1.exe"
-    Invoke-WebRequest -Uri $pythonDownloadLink -OutFile $pythonLocalName
-    Start-Process -FilePath $pythonLocalName -Args "/quiet InstallAllUsers=0 InstallLauncherAllUsers=0 PrependPath=1 Include_test=0" -Wait -NoNewWindow
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName
 } 
 
-$apodPicturesPath = Join-Path $userPictures "Apod"
-if (-not (Test-Path -Path $apodPicturesPath)) {
-    New-Item -Path $apodPicturesPath -ItemType Directory | Out-Null
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+
+public class Wallpaper {
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+}
+"@
+$SPI_SETDESKWALLPAPER = 0x0014
+$SPIF_UPDATEINIFILE = 0x01
+$SPIF_SENDCHANGE = 0x02
+
+$RegPath = "HKCU:\Control Panel\Desktop"
+$apodSaveFolder = "$env:USERPROFILE\Pictures\Apod\"
+
+if (-not (Test-Path -Path $apodSaveFolder)) {
+    New-Item -Path "$env:USERPROFILE\Pictures\" -Name "Apod" -ItemType "directory"
 }
 
-$apodDocumentsPath = Join-Path $userDocuments "Apod"
-if (-not (Test-Path -Path $apodDocumentsPath)) {
-    New-Item -Path $apodDocumentsPath -ItemType Directory | Out-Null
+$currentDate = Get-Date -Format "dd-MM-yyyy"
+$savedImagesToday = Get-ChildItem -Path $apodSaveFolder -Filter "*$currentDate.jpg"
+
+if ($savedImagesToday.Count -eq 0) { 
+    $headers = @{
+        "Accept" = "application/json"
+    }
+    $reqUrl = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+    $response = Invoke-RestMethod -Uri $reqUrl -Method Get -Headers $headers  
+
+    if ($response.media_type -eq "image" -and $response.hdurl -ne $null -and $response.title -ne $null) {
+        $imageName = $response.title
+        $imageName = $response.title -replace "[\\/:*?`"<>|]+", ""
+        $sdImage = $response.url
+        $hdImage = $response.hdurl
+        $backgroundimage = Join-Path -Path $apodSaveFolder -ChildPath ($imageName + " - " + $currentDate + ".jpg")
+
+        try {
+			Invoke-WebRequest $hdImage -OutFile $backgroundimage
+		} catch {
+			Write-Output "HD image download failed, likely the URL provided doesnt work."
+            Write-Output "Attempting to download the SD image instead."
+			Invoke-WebRequest $sdImage -OutFile $backgroundimage
+		}
+        [Wallpaper]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $backgroundimage, $SPIF_UPDATEINIFILE -bor $SPIF_SENDCHANGE)
+
+    }
+    else {
+        Write-Host "No image today. Maybe a nice video instead."
+
+        $backupImageName = "The Cone Nebula from Hubble"
+        $backupImageURL = "https://apod.nasa.gov/apod/image/2402/cone_hubbleschmidt_4048.jpg"
+        $backupBackgroundimage = Join-Path -Path $apodSaveFolder -ChildPath ($backupImageName + "_" + $currentDate + ".jpg")
+
+        Invoke-WebRequest $backupImageURL -OutFile $backupBackgroundimage
+        [Wallpaper]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $backupBackgroundimage, $SPIF_UPDATEINIFILE -bor $SPIF_SENDCHANGE)
+    }
 }
-
-cd $apodDocumentsPath
-
-$apodPythonFile = @'
-import http.client
-import ctypes
-import json
-import urllib.request
-from urllib.error import URLError, HTTPError
-import os
-from pathlib import Path
-import re
-import datetime
-import ssl
-
-currentDate = datetime.date.today()
-apodPath = Path.home() / "Pictures/Apod"
-apiKey = os.getenv('Apod')
-
-def noApiError():
-    WS_EX_TOPMOST = 0x40000
-    windowTitle = "No API Key"
-    message = "It doesn't appear as though there is an API key to use the NASA Api. You will need to add this into your operating system to work. "
-    message += "Open the Run dialog and search for SystemPropertiesAdvanced "
-    message += "Then click on Environment Variables > New "
-    message += "Set the Variable Name as Apod and the value as your API key "
-    ctypes.windll.user32.MessageBoxExW(None, message, windowTitle, WS_EX_TOPMOST)
-
-if apiKey is None:
-    noApiError()
-
-# check if todays image has already been downloaded, if yes then quit out
-def checkFileDates(apodPath):
-    for fileName in os.listdir(apodPath):
-        filePath = os.path.join(apodPath, fileName)
-        if os.path.isfile(filePath):
-            createdTimestamp = os.path.getctime(filePath)
-            createdDate = datetime.date.fromtimestamp(createdTimestamp)
-            createdTime = datetime.datetime.fromtimestamp(createdTimestamp).time()
-            eightAm = datetime.time(8, 0)  # 8:00 AM
-
-            if createdDate == currentDate and createdTime >= eightAm:
-                return True
-    return False
-
-if checkFileDates(apodPath):
-    exit()
-
-def removeInvalidPathChars(img):
-    invalidChars = '[<>:"/\\\\|?*]'
-    newImgName = re.sub(invalidChars, '', img)
-    return newImgName
-
-print("Updating Wallpaper With Today's Astronomy Picture Which Is Of")
-
-# Make API Request
-apiKey = os.getenv('Apod')
-conn = http.client.HTTPSConnection(
-    "api.nasa.gov",
-    context = ssl._create_unverified_context()
-)
-headersListJson = {
- "Accept": "application/json" 
+else {
+    $savedImage = Join-Path -Path $apodSaveFolder -ChildPath ($savedImagesToday.Name)
+    [Wallpaper]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $savedImage, $SPIF_UPDATEINIFILE -bor $SPIF_SENDCHANGE)
+    Write-Host "Image for today already exists."
 }
-payload = ""
-conn.request("GET", "/planetary/apod?api_key=" + apiKey, payload, headersListJson)
-response = conn.getresponse()
-result = response.read()
-data = json.loads(result.decode("utf-8"))
-
-#ssl._create_default_https_context = ssl._create_unverified_context #Uncomment this line if you get an SSL error.
-# Check if Apod today is video or image
-if (data["media_type"] != "video"):
-    imgName = data["title"]
-    imgName = removeInvalidPathChars(imgName)
-    imgURL = data["hdurl"]
-    altImgURL = data["url"]
-    print(imgName)
-    # Download Apod image
-    localPath = os.path.join(apodPath, imgName + ".jpg")
-    try:
-        urllib.request.urlretrieve(imgURL, localPath)
-    except HTTPError as error:
-        if error.code == 404:
-            try:
-                urllib.request.urlretrieve(altImgURL, localPath)
-            except URLError as error:
-                print(f"Failed to download the SD image. Error: {error}")
-                imgName = "NGC 6960 The Witch's Broom Nebula"
-                imgURL = "https://apod.nasa.gov/apod/image/1804/ngc6960_Pugh_2000.jpg"
-                localPath = os.path.join(apodPath, imgName + ".jpg")
-                urllib.request.urlretrieve(imgURL, localPath)
-        else:
-            print(f"HTTP Error: {error}")
-    except URLError as error:
-        print(f"Failed to download the HD image. Error: {error}")
-else: 
-    imgName = "NGC 6960 The Witch's Broom Nebula"
-    imgURL = "https://apod.nasa.gov/apod/image/1804/ngc6960_Pugh_2000.jpg"
-
-# Set image as background
-class Main:
-    def __init__(self):
-        path = os.path.join(apodPath, imgName + ".jpg")
-        ctypes.windll.user32.SystemParametersInfoW(20, 0, path , 0)
-application = Main()
-
-exit()
-'@
-
-$apodBatFile = @'
-@echo off
-py apod.py
-'@
-
-$pythonFilePath = Join-Path -Path $userDocuments\Apod -ChildPath "apod.py"
-$batFilePath = Join-Path -Path $userDocuments\Apod -ChildPath "apod.bat"
-
-New-Item -Path $pythonFilePath -Type File -Value $apodPythonFile -Force
-New-Item -Path $batFilePath -Type File -Value $apodBatFile -Force
-
-$userName = $Env:UserName
-$pcName = $env:COMPUTERNAME
-
-$taskTrigger = New-ScheduledTaskTrigger -AtLogOn
-$taskAction = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c C:\Users\$username\Documents\Apod\apod.bat"
-$taskPrincipal = New-ScheduledTaskPrincipal -UserId "$pcName\$userName" -LogonType Interactive -RunLevel Highest
-Register-ScheduledTask "Apod" -Action $taskAction -Trigger $taskTrigger -Principal $taskPrincipal
-
-$promptToInputApiKey = "You will now need to input your Nasa API key into your system variables`n`n"
-$promptToInputApiKey += "Open the Run dialog by holding the Windows Key and press R, then search for SystemPropertiesAdvanced`n`n"
-$promptToInputApiKey += "Click on Environment Variables > New`n`n"
-$promptToInputApiKey += "Set the Variable Name as Apod and the value as your API key from https://api.nasa.gov/"
-
-[System.Windows.Forms.MessageBox]::Show($promptToInputApiKey, "Finished")
--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-Install <a href="https://www.python.org/downloads/">Python</a><br>
-Generate an API key at <a href="https://api.nasa.gov/">https://api.nasa.gov/</a><br>
-Hold CTRL R to open the Run window and search for <strong>SystemPropertiesAdvanced</strong><br>
-Select Environment Variables > New<br>
-Name your variable <strong>Apod</strong> and input your API key in the value<br>
-Create a scheduled task to run this script on logon<br>
-Alternatively, run the Apod.ps1 script to set everything up except the API key
+This is just a simple script to set your Windows wallpaper background to the <a href="https://apod.nasa.gov/apod/astropix.html">Nasa Astronomy Picture of the Day (APOD)</a>.
+
+The script will create a scheduled task targeting itself to run on logon so place the script somewhere it won't be accidently deleted. On first run this will request administrator privileges to create the task. You can create the scheduled task manually to avoid this if preferred.
+
+The scheduled task will be found in Task Scheduler Library > Apod and can be altered if required to point the script at a different path or to run on a different schedule.
+
+By default this will save the APOD to C:\Users\%USERNAME%\Pictures\Apod.
+This folder is never automatically emptied and can grow quite large in size if you wish to keep each picture for future backgrounds.
+
+There can be instances where the APOD is instead a video, to handle this I've set a previous APOD I liked to be used instead.
